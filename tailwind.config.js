ee1d3f21fce13ab9f2a89edecbb126131fefd70f
myogaazwar <yogaazwar123@gmail.com>
2024-10-01 21:04:10 +0700

    add: extends color and width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 'main-color': '#FFB603' },
+      maxWidth: {
+        desktop: '1440px',
+      },
+    },
   },
   plugins: [],
 };
