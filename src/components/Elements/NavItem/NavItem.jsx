f0d9801586ebdcda213f1979a9906e621af71350
myogaazwar <yogaazwar123@gmail.com>
2024-10-02 13:11:38 +0700

    create: nav item component element

---
+++ src/components/Elements/NavItem/NavItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+
+export const NavItem = ({ to, children, classname, onSelect }) => {
+  return (
+    <li>
+      <ScrollLink
+        to={to}
+        smooth={true}
+        duration={400}
+        spy={true}
+        hashSpy={true}
+        offset={-50}
+        activeClass='md:text-main-color text-white text-bold'
+        className={`${classname} cursor-pointer`}
+        onClick={onSelect}
+      >
+        {children}
+      </ScrollLink>
+    </li>
+  );
+};
