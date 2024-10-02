aaa33d60103c8e25a6709a820f341ee6a3f39916
myogaazwar <yogaazwar123@gmail.com>
2024-10-02 21:14:05 +0700

    update: high full galeri section

--- src/components/galeri/Galeri.jsx
+++ src/components/galeri/Galeri.jsx
@@ -11,7 +11,7 @@ const Galeri = () => {
   return (
     <section
       id={'galeri'}
-      className='h-[1200px] px-8 py-2 mt-20  eb-garamond-medium'
+      className='h-full px-8 py-2 mt-20  eb-garamond-medium'
     >
       <h2 className='mb-5 text-center text-2xl'>GALERI</h2>
       <Swiper
