14716d74b68762a0f3c9f9ee1d88c1810e8d7a94
myogaazwar <yogaazwar123@gmail.com>
2024-10-02 13:08:00 +0700

    create: route Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/Home';
+import { Navbar } from './components/navbar/Navbar';
+
 function App() {
-  return <div className='text-red-500'>RT-002</div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
