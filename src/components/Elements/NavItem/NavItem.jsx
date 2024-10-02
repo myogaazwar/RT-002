c8f0c0e3433b2796a1b17c722d870556619b3d09
myogaazwar <yogaazwar123@gmail.com>
2024-10-02 19:37:32 +0700

    change: offset nav item

--- src/components/Elements/NavItem/NavItem.jsx
+++ src/components/Elements/NavItem/NavItem.jsx
@@ -10,7 +10,7 @@ export const NavItem = ({ to, children, classname, onSelect }) => {
         duration={400}
         spy={true}
         hashSpy={true}
-        offset={-50}
+        offset={-20}
         activeClass='md:text-main-color text-white text-bold'
         className={`${classname} cursor-pointer`}
         onClick={onSelect}
