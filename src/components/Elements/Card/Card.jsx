ac11703087cdf7590042d9e165ec65d43471c40c
myogaazwar <yogaazwar123@gmail.com>
2024-10-02 19:31:10 +0700

    update: w-full card image

--- src/components/Elements/Card/Card.jsx
+++ src/components/Elements/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ title, image, description }) => {
   return (
     <div className='bg-white px-3 py-4 rounded-md shadow-md -inset-10 hover:scale-105 hover:shadow-lg transition-transform'>
-      <img className='rounded-md' src={image} alt={title} />
+      <img className='rounded-md w-full' src={image} alt={title} />
       <div className='flex flex-col gap-y-3 mt-2'>
         <h3 className='eb-garamond-bold'>{title}</h3>
         <p className='text-justify text-sm'>{description}</p>
