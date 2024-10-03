1060484a24217d0b3495b17e9f4b736dca3c438e
myogaazwar <yogaazwar123@gmail.com>
2024-10-03 16:07:57 +0700

    update: navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,59 +21,71 @@ export const Navbar = () => {
   };
 
   return (
-    <div className='w-full fixed eb-garamond-medium  z-50 bottom-4  md:top-0 md:relative md:shadow-none md:border-b-2 md:rounded-none'>
-      <nav className='flex justify-center max-w-desktop text-md h-16 md:mx-auto  md:h-20 md:justify-between md:py-2 md:px-8 transition-all ease-in-out duration-500 '>
-        <img className='w-32 max-md:hidden' src={logo} alt='Logo RT002' />
+    <div className=' w-full fixed eb-garamond-medium  z-50 bottom-4  md:top-0  md:shadow-none md:rounded-none max-h-max   '>
+      <div className='md:w-full md:bg-white md:border-b-2'>
+        <nav className='flex justify-center  text-md h-16 md:mx-auto  md:h-20 md:justify-between md:py-2 md:px-8 transition-all ease-in-out duration-500  md:max-w-desktop  '>
+          <img className='w-32 max-md:hidden' src={logo} alt='Logo RT002' />
 
-        <ul className='z-10 flex items-center gap-x-8 justify-center text-md text-black eb-garamond-semibold bg-main-color border-2 border-black w-full mx-5 rounded-2xl px-2 md:bg-transparent md:border-none md:rounded-none md:px-0 md:text-xl transition-transform duration-500 '>
-          <NavItem to={'home'}>
-            <IconContainer>
-              <IoHomeOutline />
-            </IconContainer>
-            Home
-          </NavItem>
-          <NavItem to={'kegiatan'}>
-            <IconContainer>
-              <FaTasks />
-            </IconContainer>
-            Kegiatan
-          </NavItem>
-          <NavItem to={'tentang'} onSelect={() => handleDropdown('tentang')}>
-            <IconContainer>
-              <FaPeopleGroup />
-            </IconContainer>
-            Tentang
-          </NavItem>
-          <NavItem to={'galeri'}>
-            <IconContainer>
-              <GrGallery />
-            </IconContainer>
-            Galeri
-          </NavItem>
-        </ul>
-
-        <div
-          id='dropdown-tentang'
-          className={` ${
-            openDropdown === 'tentang'
-              ? ' opacity-100 translate-x-12 -translate-y-16 h-16   '
-              : 'opacity-0 translate-x-12 translate-y-0 h-16  '
-          } md:hidden absolute mb-2 bg-main-color rounded-b-2xl rounded-t-xl w-40  transition duration-400 ease-linear transform border-t-2 border-r-2 border-l-2 border-black  `}
-        >
-          <ul className='text-center py-2'>
-            <li>
-              <a href=''>Struktur Organisasi</a>
-            </li>
-            <li>
-              <a href=''>Prestasi & Pencapaian</a>
-            </li>
+          <ul className='z-10 flex items-center gap-x-8 justify-center text-md text-black eb-garamond-semibold bg-main-color border-2 border-black w-full mx-5 rounded-2xl px-2 md:bg-transparent md:border-none md:rounded-none md:px-0 md:text-xl  transition-transform duration-500 '>
+            <NavItem to={'home'}>
+              <IconContainer>
+                <IoHomeOutline />
+              </IconContainer>
+              Home
+            </NavItem>
+            <NavItem to={'kegiatan'}>
+              <IconContainer>
+                <FaTasks />
+              </IconContainer>
+              Kegiatan
+            </NavItem>
+            <NavItem to={'tentang'} onSelect={() => handleDropdown('tentang')}>
+              <IconContainer>
+                <FaPeopleGroup />
+              </IconContainer>
+              Tentang
+            </NavItem>
+            <NavItem to={'galeri'}>
+              <IconContainer>
+                <GrGallery />
+              </IconContainer>
+              Galeri
+            </NavItem>
           </ul>
-        </div>
 
-        <div className='flex items-center max-md:hidden'>
-          <Button classname={'eb-garamond-bold '}>MASUK</Button>
-        </div>
-      </nav>
+          <div
+            id='dropdown-tentang'
+            className={` ${
+              openDropdown === 'tentang'
+                ? ' opacity-100 translate-x-12 -translate-y-20 h-20   '
+                : 'opacity-0 translate-x-12 translate-y-0 h-20  '
+            } md:hidden absolute mb-2 bg-main-color rounded-b-2xl rounded-t-xl w-40  transition duration-400 ease-linear transform border-t-2 border-r-2 border-l-2 border-black  `}
+          >
+            <ul className='text-center py-2 flex flex-col gap-y-2'>
+              <li className='hover:text-white active:text-white'>
+                <a className='underline underline-offset-4' href=''>
+                  Struktur Organisasi
+                </a>
+              </li>
+              <li className='hover:text-white active:text-white'>
+                <a className='underline underline-offset-4' href=''>
+                  Prestasi & Pencapaian
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className=' items-center hidden md:flex'>
+            <Button
+              classname={
+                'eb-garamond-bold bg-main-color text-black active:text-white hover:scale-105 '
+              }
+            >
+              MASUK
+            </Button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
