0e63946c926bda6dfeeed9b8ecbf4386228b4858
myogaazwar <yogaazwar123@gmail.com>
2024-10-03 16:07:18 +0700

    update: galeri responsive

--- src/components/galeri/Galeri.jsx
+++ src/components/galeri/Galeri.jsx
@@ -11,14 +11,20 @@ const Galeri = () => {
   return (
     <section
       id={'galeri'}
-      className='h-full px-8 py-2 mt-20  eb-garamond-medium'
+      className='h-full px-8 py-2 mt-20  eb-garamond-medium max-w-desktop mx-auto'
     >
       <h2 className='mb-5 text-center text-2xl'>GALERI</h2>
       <Swiper
         navigation={true}
         modules={[Navigation]}
         spaceBetween={50}
-        className='custom-swiper'
+        className='custom-swiper '
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
       >
         <SwiperSlide>
           <img className='w-full' src={galeri1} alt='' />
@@ -26,6 +32,13 @@ const Galeri = () => {
         <SwiperSlide>
           <img className='w-full' src={galeri1} alt='' />
         </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full' src={galeri1} alt='' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full' src={galeri1} alt='' />
+        </SwiperSlide>
+
         <SwiperSlide>
           <img className='w-full' src={galeri1} alt='' />
         </SwiperSlide>
