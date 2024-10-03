120e8d64c87772bf98ae8eb6b541957146adf027
myogaazwar <yogaazwar123@gmail.com>
2024-10-03 16:07:42 +0700

    update: kegiatan responsive

--- src/components/kegiatan/Kegiatan.jsx
+++ src/components/kegiatan/Kegiatan.jsx
@@ -9,15 +9,15 @@ const Kegiatan = () => {
     <section id={'kegiatan'} className='h-full eb-garamond-medium mt-32 '>
       <main>
         <div>
-          <h2 className='text-2xl text-center'>KEGIATAN</h2>
-          <p className='text-sm mt-2 text-justify'>
+          <h2 className='text-2xl text-center md:text-3xl'>KEGIATAN</h2>
+          <p className='text-sm mt-2 text-justify md:text-center md:text-lg'>
             Setiap kegiatan yang kami lakukan berlandaskan semangat kebersamaan
             untuk menjaga kebersihan, keamanan, dan kenyamanan lingkungan RT002.
             Bersama kita bisa!
           </p>
         </div>
 
-        <div className='mt-10 grid gap-y-4'>
+        <div className='mt-10 grid gap-y-4 md:grid-cols-3 md:gap-x-4 lg:gap-x-10'>
           {semuaKegiatan.map((kegiatan) => (
             <Card
               key={kegiatan.id}
@@ -26,8 +26,13 @@ const Kegiatan = () => {
               description={kegiatan.description}
             />
           ))}
-
-          <Button classname={'w-full bg-gray-200 mt-5'}>
+        </div>
+        <div className='flex items-center justify-center'>
+          <Button
+            classname={
+              'w-full bg-gray-200 mt-10 md:w-52 hover:scale-105 hover:bg-gray-300'
+            }
+          >
             Lihat kegiatan lainnya..
           </Button>
         </div>
