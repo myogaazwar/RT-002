f179b8376c0c312898925bd0e14e5097eba491ca
myogaazwar <yogaazwar123@gmail.com>
2024-10-04 00:14:07 +0700

    update: app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/Home';
+import NotFound from './pages/NotFound';
 import { Navbar } from './components/navbar/Navbar';
+import KegiatanPage from './pages/Kegiatan';
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar />
         <Routes>
-          <Route path='/' element={<HomePage />}></Route>
+          <Route
+            path='/'
+            element={
+              <MainLayout>
+                <HomePage />
+              </MainLayout>
+            }
+          ></Route>
+          <Route
+            path='/kegiatan'
+            element={
+              <MainLayout>
+                <KegiatanPage />
+              </MainLayout>
+            }
+          ></Route>
+          <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </Router>
     </div>
