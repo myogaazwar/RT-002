582c3b4affe5dcc95b5a0640ceb825b7111cd4ec
myogaazwar <yogaazwar123@gmail.com>
2024-10-03 16:06:37 +0700

    update: card responsive

--- src/components/Elements/Card/Card.jsx
+++ src/components/Elements/Card/Card.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Card = ({ title, image, description }) => {
   return (
-    <div className='bg-white px-3 py-4 rounded-md shadow-md -inset-10 hover:scale-105 hover:shadow-lg transition-transform'>
-      <img className='rounded-md w-full' src={image} alt={title} />
-      <div className='flex flex-col gap-y-3 mt-2'>
-        <h3 className='eb-garamond-bold'>{title}</h3>
-        <p className='text-justify text-sm'>{description}</p>
+    <div className=' bg-white px-3 py-4 rounded-md shadow-md  hover:scale-105 hover:shadow-lg transition-transform md:flex md:flex-col md:justify-between md:h-full'>
+      <img
+        className='rounded-md w-full  md:object-cover md:h-48 lg:object-contain lg:h-full '
+        src={image}
+        alt={title}
+      />
+      <div className='flex flex-col flex-grow gap-y-3 mt-2 md:justify-start md:flex  md:mt-4 '>
+        <h3 className='eb-garamond-bold md:text-lg lg:text-xl'>{title}</h3>
+        <p className='text-justify text-sm lg:text-lg '>{description}</p>
       </div>
     </div>
   );
