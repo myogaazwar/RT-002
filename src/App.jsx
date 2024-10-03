a3b7e5c03a005490c4097abd241798c92a125952
myogaazwar <yogaazwar123@gmail.com>
2024-10-04 00:45:25 +0700

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HomePage } from './pages/Home';
 import NotFound from './pages/NotFound';
 import { Navbar } from './components/navbar/Navbar';
 import KegiatanPage from './pages/Kegiatan';
+import StrukturRtPage from './pages/StrukturRt';
+import Footer from './components/footer/Footer';
 
 const MainLayout = ({ children }) => {
   return (
@@ -25,15 +27,26 @@ function App() {
                 <HomePage />
               </MainLayout>
             }
-          ></Route>
+          />
           <Route
             path='/kegiatan'
             element={
               <MainLayout>
                 <KegiatanPage />
+                <Footer />
               </MainLayout>
             }
-          ></Route>
+          />
+          <Route
+            path='/struktur-rt'
+            element={
+              <MainLayout>
+                <StrukturRtPage />
+                <Footer />
+              </MainLayout>
+            }
+          />
+
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </Router>
