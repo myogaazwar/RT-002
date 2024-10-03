b8129498035a7f54de6a98ea8d8b2c1891a7d364
myogaazwar <yogaazwar123@gmail.com>
2024-10-04 00:45:36 +0700

    create: setup struktur rt page

---
+++ src/pages/StrukturRt.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StrukturRtPage = () => {
+  return (
+    <section className='h-full eb-garamond-medium px-8 mx-auto pt-10 pb-40 max-w-desktop md:pt-28'>
+      <div>
+        <h1>Ini Halaman Struktur RT</h1>
+      </div>
+    </section>
+  );
+};
+
+export default StrukturRtPage;
