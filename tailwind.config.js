5ab7800c5ca01582888083562638b6074c717329
myogaazwar <yogaazwar123@gmail.com>
2024-10-06 19:18:37 +0700

    add: config color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: { 'main-color': '#FFB603' },
+      colors: {
+        'main-color': '#FFB603',
+        'dark-mode': '#0C0C0C',
+        'second-color': '#1B1A55',
+      },
       maxWidth: {
         desktop: '1440px',
       },
     },
   },
   plugins: [],
+  darkMode: 'class',
 };
