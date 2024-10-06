fd49f04e0ac854e9ab892b3fe819b54b76b56d3a
myogaazwar <yogaazwar123@gmail.com>
2024-10-06 19:22:20 +0700

    add: jumbotron for dark mode

--- src/components/jumbotron/Jumbotron.jsx
+++ src/components/jumbotron/Jumbotron.jsx
@@ -6,23 +6,23 @@ const Jumbotron = () => {
   return (
     <section
       id={'/'}
-      className='h-full  eb-garamond-medium pb-20 pt-2 md:pt-0 '
+      className='h-full  eb-garamond-medium pb-20 pt-2 md:pt-0  '
     >
       <div className='flex flex-col-reverse mt-5 gap-y-10 md:flex-row md:justify-between md:items-center md:gap-x-4 md:py-10 md:h-[800px]'>
-        <div className=' text-center flex flex-col gap-y-5 md:text-left'>
-          <div className='text-4xl lg:text-5xl'>
+        <div className=' text-center flex flex-col gap-y-5 md:text-left '>
+          <div className='text-4xl lg:text-5xl dark:text-white'>
             <h1>SELAMAT DATANG</h1>
             <h1>DI RT 002/RW 010</h1>
           </div>
 
-          <p className='eb-garamond-regular-italic lg:text-xl'>
+          <p className='eb-garamond-regular-italic lg:text-xl dark:text-white'>
             Bersama, Membangun Lingkungan Yang Harmonis dan Aman
           </p>
 
           <div>
             <Button
               classname={
-                'bg-main-color w-full text-lg eb-garamond-bold md:w-[300px]  '
+                'bg-main-color dark:bg-second-color dark:text-white w-full text-lg eb-garamond-bold md:w-[300px]  '
               }
             >
               MASUK SEKARANG
