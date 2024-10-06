176f15e9824a86fecc970b3370371f430db152ff
myogaazwar <yogaazwar123@gmail.com>
2024-10-06 19:22:03 +0700

    create context theme dark mode at app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import KegiatanPage from './pages/Kegiatan';
 import StrukturRtPage from './pages/StrukturRt';
 import Footer from './components/footer/Footer';
 import PrestasiPage from './pages/Prestasi';
-import { useEffect } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import GaleriPage from './pages/Galeri';
 
 const ScrollToTop = () => {
@@ -25,69 +25,81 @@ const ScrollToTop = () => {
 };
 
 const MainLayout = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <>
+    <div className={` ${theme ? 'dark' : 'light'}`}>
       <Navbar />
-      <main>{children}</main>
-    </>
+      <main className='dark:bg-dark-mode'>{children}</main>
+    </div>
   );
 };
 
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <div>
       <Router>
         <ScrollToTop />
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <MainLayout>
-                <HomePage />
-              </MainLayout>
-            }
-          />
-          <Route
-            path='/kegiatan'
-            element={
-              <MainLayout>
-                <KegiatanPage />
-                <Footer />
-              </MainLayout>
-            }
-          />
-          <Route
-            path='/struktur-rt'
-            element={
-              <MainLayout>
-                <StrukturRtPage />
-                <Footer />
-              </MainLayout>
-            }
-          />
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <MainLayout>
+                  <HomePage />
+                </MainLayout>
+              }
+            />
+            <Route
+              path='/kegiatan'
+              element={
+                <MainLayout>
+                  <KegiatanPage />
+                  <Footer />
+                </MainLayout>
+              }
+            />
+            <Route
+              path='/struktur-rt'
+              element={
+                <MainLayout>
+                  <StrukturRtPage />
+                  <Footer />
+                </MainLayout>
+              }
+            />
 
-          <Route
-            path='/prestasi'
-            element={
-              <MainLayout>
-                <PrestasiPage />
-                <Footer />
-              </MainLayout>
-            }
-          />
+            <Route
+              path='/prestasi'
+              element={
+                <MainLayout>
+                  <PrestasiPage />
+                  <Footer />
+                </MainLayout>
+              }
+            />
 
-          <Route
-            path='/galeri'
-            element={
-              <MainLayout>
-                <GaleriPage />
-                <Footer />
-              </MainLayout>
-            }
-          />
+            <Route
+              path='/galeri'
+              element={
+                <MainLayout>
+                  <GaleriPage />
+                  <Footer />
+                </MainLayout>
+              }
+            />
 
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </ThemeContext.Provider>
       </Router>
     </div>
   );
